54b0b0c49ee73bcc0c20e70c8462db809d6cd68d
Adam Sturrock <131348113+AdamJSturrock@users.noreply.github.com>
2024-10-09 12:48:50 +0100

    update User table schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,24 +1,58 @@
-//user model
+// user model
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define( "user", {
-        userName: {
-            type: DataTypes.STRING,
+    const User = sequelize.define("User", {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
             allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        gh_username: {
+            type: DataTypes.STRING,
+            allowNull: true
         },
         email: {
             type: DataTypes.STRING,
+            allowNull: false,
             unique: true,
-            isEmail: true, //checks for email format
-            allowNull: false
+            validate: {
+                isEmail: true // Checks for valid email format
+            }
+        },
+        emailVerified: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: true
         },
         password: {
             type: DataTypes.STRING,
             allowNull: false
         },
-        role: {
-            type: DataTypes.STRING,
-            allowNull: false
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
         }
-    }, {timestamps: true}, )
-    return User
-}
+    }, {
+        timestamps: true,  // Automatically adds createdAt and updatedAt fields
+        tableName: 'User' // Specify the actual table name
+    });
+
+    return User;
+};
